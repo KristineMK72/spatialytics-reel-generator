44d250df9bb5ab1d2ba78cf048b4885ace0eb4b0
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-03-08 13:30:37 -0500

    Create types.ts

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+export type PromoVideoProps = {
+  headline: string;
+  subheadline: string;
+  bullets: string[];
+  cta: string;
+  website: string;
+  backgroundUrl?: string;
+  promoImageUrl?: string;
+  logoUrl?: string;
+};
